de7a9e02e9c291da4bd84e7b77825871924e5e95
neg2led <4232981+neg2led@users.noreply.github.com>
2020-12-19 13:11:41 +1100

    update setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# make venv if not exist
+if [ ! -d venv ]; then
+    virtualenv -p python3 venv
+fi
+
+# set environment variables
+export VENV=$(pwd)/venv
+
+# activate venv
+source $VENV/bin/activate
+
+# update pip and setuptools
+venv/bin/pip install --upgrade pip setuptools
+
+# install local ncclient
+venv/bin/pip install -e ./ncclient
